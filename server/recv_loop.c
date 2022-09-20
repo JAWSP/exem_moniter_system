2f193716ae5197d0502564292ef4619db0183cc8
박주호 <juhpark@bagjuhoui-MacBookPro.local>
2022-09-21 08:11:44 +0900

    fix wrong byte

--- server/recv_loop.c
+++ server/recv_loop.c
@@ -49,7 +49,7 @@ int get_recv(char *buf, int size, agentInfo *ag, squeue *q, phase *phase)
 
 		curr_size = 0;
 		full_size = 0;
-		*phase = PHASE_AFTER;
+		phase++;
 	}
 	return (0);
 }
@@ -61,7 +61,7 @@ void *pth_server_loop(void *arg)
 	int agent_fd = gs->agent_fd;
 	int size;
 	squeue *q = (squeue *)arg;
-	phase *phase;
+	phase phase;
 
 	int res = 0;
 	pthread_mutex_lock(&gs->g_comu_lock);
@@ -77,41 +77,45 @@ void *pth_server_loop(void *arg)
 	}
 	pthread_mutex_unlock(&gs->g_comu_lock);
 
-	*phase = PHASE_BEFORE;
+	phase = PHASE_READY;
 	while (1)
 	{
 		struct sockaddr_in agt_adr;
 		socklen_t agt_adr_len;
 		agentInfo *ag;
-		if (!(ag = (agentInfo *)malloc(sizeof(agentInfo))))
-			err_by("agent info malloc error");
-		
-		agt_adr_len = sizeof(agt_adr);
 		int ress = 0;
-		if (*phase == PHASE_BEFORE)
+		if (phase == PHASE_READY)
+		{
+			if (!(ag = (agentInfo *)malloc(sizeof(agentInfo))))
+				err_by("agent info malloc error");
+			
+			agt_adr_len = sizeof(agt_adr);
+			phase = PHASE_BEFORE;
+		}
+		if (phase == PHASE_BEFORE)
 		{
 			if ((ress = (recvfrom(gs->usock, ubuf, 256, 0, (struct sockaddr *)&agt_adr, &agt_adr_len))) < 0)
 				err_by("before packet recv error");
 			printf("before : %d\n", res);
-			*phase = PHASE_MAIN;
+			phase = PHASE_MAIN;
 		}
 		ag->indx = res;
-		if (*phase == PHASE_MAIN)
+		if (phase == PHASE_MAIN)
 		{
 			if ((size = recv(agent_fd, buf, 1024 *256, 0)) > 0)
 			{
-				if (get_recv(buf, size, ag, q, phase) == -1)
+				if (get_recv(buf, size, ag, q, &phase) == -1)
 					break ;
 			}
 			else
 				break ; //연결이 끊기면 루프끝->스레드끝
 		}
-		if (*phase == PHASE_AFTER)
+		if (phase == PHASE_AFTER)
 		{
 			if ((ress = (recvfrom(gs->usock, ubuf, 256, 0, (struct sockaddr *)&agt_adr, &agt_adr_len))) < 0)
 				err_by("before packet recv error");
 			printf("after : %d\n", ress);
-			*phase = PHASE_BEFORE;
+			phase = PHASE_BEFORE;
 		}
 	}
 	return ((void*)0);
